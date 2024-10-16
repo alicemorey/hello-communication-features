f3807d6c4c7fa3274a4a3fb6894a7a2cf702ab09
alicemorey <alicemmorey@gmail.com>
2024-10-16 16:04:27 +0200

    edit

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
 import { useState } from 'react';
 import { StyleSheet, View, Button, Image } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
+import * as Location from 'expo-location';
+import MapView from 'react-native-maps';
 
 const App = () => {
   const [image, setImage] = useState(null);
+  const [location, setLocation] = useState(null);
 
 
 const pickImage = async () => {
@@ -15,7 +18,29 @@ const pickImage = async () => {
     if (!result.canceled) setImage(result.assets[0]);
     else setImage(null)
   }
-}
+};
+  const takePhoto = async () => {
+    let permissions = await ImagePicker.requestCameraPermissionsAsync();
+
+    if (permissions?.granted) {
+      let result = await ImagePicker.launchCameraAsync();
+
+      if (!result.canceled) setImage(result.assets[0]);
+      else setImage(null)
+    }
+};
+
+const getLocation = async () => {
+  let permissions = await Location.requestForegroundPermissionsAsync();
+
+  if (permissions?.granted) {
+    const location = await Location.getCurrentPositionAsync({});
+    setLocation(location);
+  } else {
+    Alert.alert("Permissions to read location aren't granted");
+  }
+};
+
   return (
     <View style={styles.container}>
       <Button
@@ -24,12 +49,27 @@ const pickImage = async () => {
       />
       <Button
         title="Take a photo"
-        onPress={() => {}}
+        onPress={takePhoto}
+      />
+
+    <Button
+      title="Get my location"
+      onPress={getLocation}
       />
 
       {image &&
   <Image source={{ uri: image.uri }} style={{ width: 200, height: 200 }} />
 }
+{location &&
+  <MapView
+    style={{width: 300, height: 200}}
+    region={{
+      latitude: location.coords.latitude,
+      longitude: location.coords.longitude,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421,
+    }}
+/>}
     </View>
   );
 }
